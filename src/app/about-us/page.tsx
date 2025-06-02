72781bbfda263db88ab17c934997f615db4ebf36
rifkyNur <ythrifky619@gmail.com>
2025-06-02 14:30:25 +0700

    finish about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/page/aboutUs/Hero";
 import AchievementAndGoal from "@/components/page/aboutUs/AchievementAndGoal";
+import Cta from "@/components/page/aboutUs/Cta";
 
 const page = () => {
   return (
     <div className="container min-h-screen">
       <Hero />
       <AchievementAndGoal />
+      <Cta />
     </div>
   );
 };
--- src/components/page/aboutUs/Cta.tsx
+++ src/components/page/aboutUs/Cta.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 const Cta = () => {
   return (
-    <div>
-      <div>
-        <h2>
-          <span>Together</span>, let's shape the future of digital innovation
+    <div className="p-7 my-12 bg-white rounded-lg flex flex-col gap-10 items-start md:flex-row md:items-center md:justify-between lg:p-14 lg:my-16">
+      <div className="max-w-[757px]">
+        <h2 className="font-semibold text-3xl mb-2 lg:text-4xl lg:mb-4">
+          <span className="text-orange-400">Together</span>, let's shape the
+          future of digital innovation
         </h2>
-        <p>
+        <p className="text-sm text-gray-700 lg:text-base">
           Join us on this exciting learning journey and unlock your potential in
           design and development.
         </p>
       </div>
-      <Button>Join Now</Button>
+      <Button className="bg-orange-400 hover:bg-orange-500">Join Now</Button>
     </div>
   );
 };
